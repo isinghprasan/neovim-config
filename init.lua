508ed193075c6fae0684b8d4d9ea04b5f5534db9
Prasan <singh.prasan.01@gmail.com>
2025-08-17 10:05:49 -0700

    Added lazy.nvim and gruvbox colortheme

--- init.lua
+++ init.lua
@@ -6,3 +6,20 @@ vim.api.nvim_create_user_command("Source", function()
 	dofile(vim.env.MYVIMRC)
 	print("Config reloaded")
 end, {})
+
+-- Use true colors (16M) if the terminal supports it
+vim.opt.termguicolors = true
+
+
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- load plugins from lua/plugins/*.lua
+require("lazy").setup("plugins")
